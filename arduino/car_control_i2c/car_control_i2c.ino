b340ade186e51f1f24d4395056b0e689519c5268
Kunal Powar <kunalpowar1203@gmail.com>
2013-11-29 10:30:51 +0530

    - created a separate code for debugging - Removed unnecessary code from the car_control_i2c

--- arduino/car_control_i2c/car_control_i2c.ino
+++ arduino/car_control_i2c/car_control_i2c.ino
@@ -1,6 +1,14 @@
 #include <Wire.h>
 #include <Servo.h>
 #define SLAVE_ADDRESS 0x50
+#define MAX_SERVO_ANGLE 150
+#define MIN_SERVO_ANGLE 40
+#define MAX_MOTOR_SPEED 170
+#define MIN_MOTOR_SPEED 60
+#define SERVO_CONTROL_PIN 6
+#define MOTOR_CONTROL_PIN 3
+static const char SERVO_CONTROL = 'S';
+static const char MOTOR_CONTROL = 'M';
 
 Servo servo;
 char number = 0;
@@ -8,23 +16,16 @@ int control_word = 0;
 int state = 0;
 int servo_angle = 90;
 int motor_speed = 50;
-int motor_control_pin = 3;
-int servo_control_pin = 6;
 boolean set_servo_speed = false;
 boolean set_motor_speed = false;
 
 void setup() {
-    Serial.begin(9600);      
-     Wire.begin(SLAVE_ADDRESS);
+    Wire.begin(SLAVE_ADDRESS);
+    Wire.onReceive(receiveData);
 
-     Wire.onReceive(receiveData);
-    Wire.onRequest(sendData);
-
-    Serial.println("Ready!");
-
-    pinMode(servo_control_pin, OUTPUT);
-    pinMode(motor_control_pin, OUTPUT);
-    servo.attach(servo_control_pin);
+    pinMode(SERVO_CONTROL_PIN, OUTPUT);
+    pinMode(MOTOR_CONTROL_PIN, OUTPUT);
+    servo.attach(SERVO_CONTROL_PIN);
 }
 
 void loop() {
@@ -37,47 +38,40 @@ void receiveData(int byteCount){
         control_word = Wire.read();
         if(set_servo_speed){
             servo_angle = control_word;
-            if(servo_angle > 150) {
-              servo_angle = 160;
-            } else if(servo_angle < 30) {
-              servo_angle = 30;
+            if(servo_angle > MAX_SERVO_ANGLE) {
+              servo_angle = MAX_SERVO_ANGLE;
+            } else if(servo_angle < MIN_SERVO_ANGLE) {
+              servo_angle = MIN_SERVO_ANGLE;
             }
             set_servo_speed = false;
             setServoAngle(servo_angle);
-            Serial.print("S");
-            Serial.println(servo_angle);
         }
         else if(set_motor_speed){
             motor_speed = control_word;
-            if (motor_speed > 200) {
-             motor_speed = 200; 
+            if (motor_speed > MAX_MOTOR_SPEED) {
+                motor_speed = MAX_MOTOR_SPEED; 
             }
-//            else if (motor_speed < 50 ) {
-//             motor_speed = 50; 
-//            }
+            else if (motor_speed < MIN_MOTOR_SPEED ) {
+                motor_speed = MIN_MOTOR_SPEED; 
+            }
             set_motor_speed = false;
             setMotorSpeed(motor_speed);
-            Serial.print('M');
-            Serial.println(motor_speed);
         }
-        else if(char(control_word) == 'S'){
+        else if(char(control_word) == SERVO_CONTROL){
             set_servo_speed = true;
         }
-        else if(char(control_word) == 'M'){
+        else if(char(control_word) == MOTOR_CONTROL){
             set_motor_speed = true;
         }
     }
 }
 
 void setMotorSpeed(int motor_speed){
-    analogWrite(motor_control_pin, motor_speed);
+    analogWrite(MOTOR_CONTROL_PIN, motor_speed);
 }
 
 void setServoAngle(int angle){
     servo.write(angle);
 }
 
-void sendData(){
-    Wire.write(number);
-}
 
