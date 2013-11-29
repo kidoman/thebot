9cf3442419cf2a684b382a2e07d18f295968cbcd
Nikesh Vora <nikesh.voratp@gmail.com>
2013-11-30 01:53:13 +0530

    Added software reset and collision fail safe.

--- arduino/main.ino
+++ arduino/main.ino
@@ -9,15 +9,21 @@
 #define MIN_SERVO_ANGLE     40
 #define MAX_MOTOR_SPEED     150
 #define MIN_MOTOR_SPEED     60
-
+#define COLLISION_YES       8
+#define COLLISION_NO        9
+#define PROXIMITY_SENSOR    1 
 #define SERVO_CONTROL_PIN   6
 #define MOTOR_CONTROL_PIN   3
+#define PROXIMITY_THRESHOLD 100
+#define RESET_PIN           12
+#define BREAK_TIME          750
 
 char number =               0;
 int control_word =          0;
 int state =                 0;
 int servo_angle =           90;
 int motor_speed =           50;
+int proximity =             0;
 boolean set_servo_speed =   false;
 boolean set_motor_speed =   false;
 
@@ -25,19 +31,41 @@ Servo servo;
 
 static const char SERVO_CONTROL = 'S';
 static const char MOTOR_CONTROL = 'M';
+static const char RESET = 'R';
 
 void setup() {
+    digitalWrite(RESET_PIN, HIGH);
     Wire.begin(SLAVE_ADDRESS);
     Wire.onReceive(receiveData);
+    Serial.begin(9600);
 
+    pinMode(RESET_PIN, OUTPUT);
     pinMode(SERVO_CONTROL_PIN, OUTPUT);
     pinMode(MOTOR_CONTROL_PIN, OUTPUT);
+    pinMode(PROXIMITY_SENSOR, INPUT);
+    pinMode(COLLISION_YES, OUTPUT);
+    pinMode(COLLISION_NO,OUTPUT);
 
+    digitalWrite(COLLISION_YES, HIGH);
+    digitalWrite(COLLISION_NO, LOW);
+    
     servo.attach(SERVO_CONTROL_PIN);
 }
 
 void loop() {
-    delay(1000);
+    proximity = analogRead(PROXIMITY_SENSOR);
+    Serial.println(proximity);
+    if(proximity > PROXIMITY_THRESHOLD){
+        digitalWrite(COLLISION_YES, HIGH);
+        digitalWrite(COLLISION_NO, LOW);
+        reset();
+    }
+    else{
+        digitalWrite(COLLISION_YES, LOW);
+        digitalWrite(COLLISION_NO, HIGH);
+    }
+    delay(20);
+
 }
 
 void receiveData(int byteCount) {
@@ -72,6 +100,8 @@ void receiveData(int byteCount) {
             set_servo_speed = true;
         } else if(char(control_word) == MOTOR_CONTROL) {
             set_motor_speed = true;
+        } else if(char(control_word) == RESET) {
+            digitalWrite(RESET_PIN, LOW);
         }
     }
 }
@@ -82,4 +112,12 @@ void setMotorSpeed(int motor_speed){
 
 void setServoAngle(int angle){
     servo.write(angle);
+}
+
+void reset(){
+    setMotorSpeed(20);
+    setServoAngle(90);
+    delay(BREAK_TIME);
+    setMotorSpeed(0);
+    while(1){}
 }
