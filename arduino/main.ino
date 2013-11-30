a2e4b6dd5cf7682e950b208a1ef571b23b710f80
Kunal Powar <kunalpowar1203@gmail.com>
2013-11-30 10:42:08 +0530

    - enabled software reset while on reset mode - refined manual collision threshold seection

--- arduino/main.ino
+++ arduino/main.ino
@@ -3,21 +3,32 @@
 #include <Wire.h>
 #include <Servo.h>
 
+//Address at which arduino is connected to I2C bus as a slave
 #define SLAVE_ADDRESS       0x50
 
+//Servo constants
 #define MAX_SERVO_ANGLE     150
 #define MIN_SERVO_ANGLE     40
+#define DEFAULT_SERVO_ANGLE 90
+
+//Motor Parameters
 #define MAX_MOTOR_SPEED     150
 #define MIN_MOTOR_SPEED     60
+
+//For Proximity Sensing 
 #define COLLISION_YES       8
 #define COLLISION_NO        9
+#define MIN_COLLISION_THRESHOLD 120
+
+//IO Pins
 #define PROXIMITY_SENSOR    1 
 #define SERVO_CONTROL_PIN   6
 #define MOTOR_CONTROL_PIN   3
-#define PROXIMITY_THRESHOLD 120
 #define RESET_PIN           12
+#define PROXIMITY_THRESHOLD_CONTROL 2
+
+//Misc
 #define BREAK_TIME          750
-#define DEFAULT_SERVO_ANGLE 90
 
 char number =               0;
 int control_word =          0;
@@ -25,9 +36,11 @@ int state =                 0;
 int servo_angle =           90;
 int motor_speed =           50;
 int proximity =             0;
+int proximity_control =     0;
+int manual_proximity_threshold =   0;
 boolean set_servo_speed =   false;
 boolean set_motor_speed =   false;
-boolean i2c_active =        true;
+boolean on_reset =        true;
 
 Servo servo;
 
@@ -46,6 +59,7 @@ void setup() {
     pinMode(PROXIMITY_SENSOR, INPUT);
     pinMode(COLLISION_YES, OUTPUT);
     pinMode(COLLISION_NO,OUTPUT);
+    pinMode(PROXIMITY_THRESHOLD_CONTROL, INPUT);
 
     digitalWrite(COLLISION_YES, HIGH);
     digitalWrite(COLLISION_NO, LOW);
@@ -55,21 +69,20 @@ void setup() {
 }
 
 void loop() {
+    manual_proximity_threshold = analogRead(PROXIMITY_THRESHOLD_CONTROL);
     proximity = analogRead(PROXIMITY_SENSOR);
-    if(proximity > PROXIMITY_THRESHOLD){
-        digitalWrite(COLLISION_YES, HIGH);
-        digitalWrite(COLLISION_NO, LOW);
+    if(proximity > MIN_COLLISION_THRESHOLD || proximity > manual_proximity_threshold){
+        turn_on_proximity_warning();
         reset();
     }
     else{
-        digitalWrite(COLLISION_YES, LOW);
-        digitalWrite(COLLISION_NO, HIGH);
+        turn_off_proximity_warning();
     }
     delay(20);
 }
 
 void receiveData(int byteCount) {
-    while(Wire.available() && i2c_active) {
+    while(Wire.available()) {
         control_word = Wire.read();
 
         if(set_servo_speed) {
@@ -96,12 +109,14 @@ void receiveData(int byteCount) {
             set_motor_speed = false;
 
             setMotorSpeed(motor_speed);
-        } else if(char(control_word) == SERVO_CONTROL) {
+        } else if(char(control_word) == SERVO_CONTROL && !(on_reset)) {
             set_servo_speed = true;
-        } else if(char(control_word) == MOTOR_CONTROL) {
+        } else if(char(control_word) == MOTOR_CONTROL && !(on_reset)) {
             set_motor_speed = true;
         } else if(char(control_word) == RESET) {
             digitalWrite(RESET_PIN, LOW);
+            set_servo_speed = false;
+            set_motor_speed = false;
         }
     }
 }
@@ -114,11 +129,21 @@ void setServoAngle(int angle){
     servo.write(angle);
 }
 
+void turn_on_proximity_warning() {
+    digitalWrite(COLLISION_YES, HIGH);
+    digitalWrite(COLLISION_NO, LOW);
+}
+
+void turn_off_proximity_warning() {
+    digitalWrite(COLLISION_YES, LOW);
+    digitalWrite(COLLISION_NO, HIGH);
+}
+
 void reset(){
     setMotorSpeed(20);
     setServoAngle(90);
     delay(BREAK_TIME);
     setMotorSpeed(0);
-    i2c_active = false;
+    on_reset = false;
     while(1){}
 }
