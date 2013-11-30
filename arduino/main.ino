ce5cdb1dd4bb733d220ded830cbda883d7f75132
Karan Misra <kidoman@gmail.com>
2013-12-01 01:33:19 +0530

    OCD

--- arduino/main.ino
+++ arduino/main.ino
@@ -4,49 +4,49 @@
 #include <Servo.h>
 
 //Address at which arduino is connected to I2C bus as a slave
-#define SLAVE_ADDRESS       0x50
+#define SLAVE_ADDRESS               0x50
 
 //Servo constants
-#define MAX_SERVO_ANGLE     150
-#define MIN_SERVO_ANGLE     40
-#define DEFAULT_SERVO_ANGLE 90
+#define MAX_SERVO_ANGLE             150
+#define MIN_SERVO_ANGLE             40
+#define DEFAULT_SERVO_ANGLE         90
 
 //Motor Parameters
-#define MAX_MOTOR_SPEED     150
-#define MIN_MOTOR_SPEED     60
+#define MAX_MOTOR_SPEED             150
+#define MIN_MOTOR_SPEED             60
 
-//For Proximity Sensing 
-#define COLLISION_YES       8
-#define COLLISION_NO        9
-#define MIN_COLLISION_THRESHOLD 120
+//For Proximity Sensing
+#define COLLISION_YES               8
+#define COLLISION_NO                9
+#define MIN_COLLISION_THRESHOLD     120
 
 //IO Pins
-#define PROXIMITY_SENSOR    1 
-#define SERVO_CONTROL_PIN   6
-#define MOTOR_CONTROL_PIN   3
-#define RESET_PIN           12
+#define PROXIMITY_SENSOR            1
+#define SERVO_CONTROL_PIN           6
+#define MOTOR_CONTROL_PIN           3
+#define RESET_PIN                   12
 #define PROXIMITY_THRESHOLD_CONTROL 2
 
 //Misc
-#define BREAK_TIME          750
+#define BREAK_TIME                  750
 
-char number =               0;
-int control_word =          0;
-int state =                 0;
-int servo_angle =           90;
-int motor_speed =           50;
-int proximity =             0;
-int proximity_control =     0;
-int manual_proximity_threshold =   0;
-boolean set_servo_speed =   false;
-boolean set_motor_speed =   false;
-boolean on_reset =        true;
+char number =                       0;
+int control_word =                  0;
+int state =                         0;
+int servo_angle =                   90;
+int motor_speed =                   50;
+int proximity =                     0;
+int proximity_control =             0;
+int manual_proximity_threshold =    0;
+boolean set_servo_speed =           false;
+boolean set_motor_speed =           false;
+boolean on_reset =                  true;
 
 Servo servo;
 
-static const char SERVO_CONTROL = 'S';
-static const char MOTOR_CONTROL = 'M';
-static const char RESET = 'R';
+static const char SERVO_CONTROL =   'S';
+static const char MOTOR_CONTROL =   'M';
+static const char RESET =           'R';
 
 void setup() {
     digitalWrite(RESET_PIN, HIGH);
@@ -63,7 +63,7 @@ void setup() {
 
     digitalWrite(COLLISION_YES, HIGH);
     digitalWrite(COLLISION_NO, LOW);
-    
+
     servo.attach(SERVO_CONTROL_PIN);
     servo.write(DEFAULT_SERVO_ANGLE);
 }
@@ -71,11 +71,10 @@ void setup() {
 void loop() {
     manual_proximity_threshold = analogRead(PROXIMITY_THRESHOLD_CONTROL);
     proximity = analogRead(PROXIMITY_SENSOR);
-    if(proximity > MIN_COLLISION_THRESHOLD || proximity > manual_proximity_threshold){
+    if (proximity > MIN_COLLISION_THRESHOLD || proximity > manual_proximity_threshold) {
         turn_on_proximity_warning();
         reset();
-    }
-    else{
+    } else {
         turn_off_proximity_warning();
     }
     delay(20);
@@ -85,19 +84,19 @@ void receiveData(int byteCount) {
     while(Wire.available()) {
         control_word = Wire.read();
 
-        if(set_servo_speed) {
+        if (set_servo_speed) {
             servo_angle = control_word;
 
-            if(servo_angle > MAX_SERVO_ANGLE) {
+            if (servo_angle > MAX_SERVO_ANGLE) {
               servo_angle = MAX_SERVO_ANGLE;
-            } else if(servo_angle < MIN_SERVO_ANGLE) {
+            } else if (servo_angle < MIN_SERVO_ANGLE) {
               servo_angle = MIN_SERVO_ANGLE;
             }
 
             set_servo_speed = false;
 
             setServoAngle(servo_angle);
-        } else if(set_motor_speed) {
+        } else if (set_motor_speed) {
             motor_speed = control_word;
 
             if (motor_speed > MAX_MOTOR_SPEED) {
@@ -109,11 +108,11 @@ void receiveData(int byteCount) {
             set_motor_speed = false;
 
             setMotorSpeed(motor_speed);
-        } else if(char(control_word) == SERVO_CONTROL && !(on_reset)) {
+        } else if (char(control_word) == SERVO_CONTROL && !(on_reset)) {
             set_servo_speed = true;
-        } else if(char(control_word) == MOTOR_CONTROL && !(on_reset)) {
+        } else if (char(control_word) == MOTOR_CONTROL && !(on_reset)) {
             set_motor_speed = true;
-        } else if(char(control_word) == RESET) {
+        } else if (char(control_word) == RESET) {
             digitalWrite(RESET_PIN, LOW);
             set_servo_speed = false;
             set_motor_speed = false;
@@ -121,11 +120,11 @@ void receiveData(int byteCount) {
     }
 }
 
-void setMotorSpeed(int motor_speed){
+void setMotorSpeed(int motor_speed) {
     analogWrite(MOTOR_CONTROL_PIN, motor_speed);
 }
 
-void setServoAngle(int angle){
+void setServoAngle(int angle) {
     servo.write(angle);
 }
 
@@ -139,11 +138,11 @@ void turn_off_proximity_warning() {
     digitalWrite(COLLISION_NO, HIGH);
 }
 
-void reset(){
+void reset() {
     setMotorSpeed(20);
     setServoAngle(90);
     delay(BREAK_TIME);
     setMotorSpeed(0);
     on_reset = false;
-    while(1){}
+    while(true) {}
 }
